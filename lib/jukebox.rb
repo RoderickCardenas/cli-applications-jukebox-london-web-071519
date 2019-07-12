1ef686b783b7cdcc0b63187afafeb5407b735473
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-12 11:32:29 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -8,9 +8,10 @@ puts "- play : lets you choose a song to play"
 puts "- exit : exits this program"
 end
 
-def list
-  # This method takes in an argument of the songs array and outputs each song in the array on a new line. A number should be listed before each song, indicating that songs track number:
-  # Use the Array#each_with_index method to access the index number of a given element as you step through the iteration. Remember that arrays are indexed starting at 0. So, adding 1 to the index number at a given step of the iteration will result in the correct song number for the purposes of outputting your list of songs.
+def list(songs)
+songs.each_with_index do |song, index|
+  puts "#{index+1}. #{song}"
+ end
 end
 
 def play(songs)
