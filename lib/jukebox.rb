a51191d9307787a3cce30088c7af12a9c3adb3f5
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-12 11:30:32 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -13,12 +13,16 @@ def list
   # Use the Array#each_with_index method to access the index number of a given element as you step through the iteration. Remember that arrays are indexed starting at 0. So, adding 1 to the index number at a given step of the iteration will result in the correct song number for the purposes of outputting your list of songs.
 end
 
-def play
-puts "Please enter a song name or number:"
-input = gets.chomp
-
-
-
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.chomp
+  if input.to_i >= 1 && input.to_i <= songs.length
+    puts "Playing #{songs[input.to_i-1]}"
+  elsif songs.include?(input)
+    puts "Playing #{songs.find{|song| song == input}}"
+  else
+    puts "Invalid input, please try again"
+  end
 end
 
 def exit_jukebox
